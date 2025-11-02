65bd90cda19c4a4976b98e6d309d7d8200438a83
nahuel <cuenta3fne@gmail.com>
2025-11-02 01:40:23 -0300

    feat(categorias): define rutas web para categorias

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/categories', function () {
+    return redirect()->route('categories.index');
 });
+
+// Rutas del CRUD de categorías
+Route::resource('categories', CategoryController::class);
+
